494b0f3da727e4495f89d505403c3c07fe1bed7f
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-01-13 00:29:26 +0000

    change token name

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Interview Buddy`,
@@ -5,19 +9,17 @@ module.exports = {
     author: `@jcheso`,
     siteUrl: `https://interview-buddy.netlify.app/`,
   },
+
   plugins: [
     {
       resolve: `gatsby-source-sanity`,
       options: {
-        projectId: `abc123`,
-        dataset: `blog`,
+        projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+        dataset: process.env.GATSBY_SANITY_DATASET,
         // a token with read permissions is required
         // if you have a private dataset
-        token: process.env.SANITY_TOKEN,
-
-        // If the Sanity GraphQL API was deployed using `--tag <name>`,
-        // use `graphqlTag` to specify the tag name. Defaults to `default`.
-        graphqlTag: "default",
+        token: process.env.GATSBY_SANITY_TOKEN,
+        watchMode: true,
       },
     },
     `gatsby-plugin-postcss`,
